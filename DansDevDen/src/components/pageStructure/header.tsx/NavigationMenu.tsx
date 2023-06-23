2f505049e59bc530b325faf4f784c1af285c19f9
danriley11 <dan_riley7@outlook.com>
2023-06-23 16:26:19 +1000

    Update naming conventions

---
+++ DansDevDen/src/components/pageStructure/header.tsx/NavigationMenu.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { HOUSEKEEPING } from '../../../routes/routes';
+import { MenuIconContainer, XBar1, XBar2, XBar3 } from '../../menu/Menu.styles';
+import { MenuDropdownContainer, MenuDropdownItem } from '../../menu/MenuDropdown.styles';
+import { P } from '../../core/typography';
+
+const NavigationMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const navigate = useNavigate();
+
+  const toggleHamburger = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <MenuIconContainer className="nav-toggle" onClick={toggleHamburger}>
+        <XBar1 isOpen={isOpen} />
+        <XBar2 isOpen={isOpen} />
+        <XBar3 isOpen={isOpen} />
+      </MenuIconContainer>
+
+      {isOpen && (
+        <MenuDropdownContainer>
+          <MenuDropdownItem onClick={() => navigate(HOUSEKEEPING)}>
+            <P>Housekeeping</P>
+          </MenuDropdownItem>
+        </MenuDropdownContainer>
+      )}
+    </>
+  );
+};
+
+export default NavigationMenu;
---
+++ DansDevDen/src/pages/grandFoyer/GrandFoyer.tsx
@@ -0,0 +1,11 @@
+import { P } from '../../components/core/typography';
+
+const GrandFoyer = () => {
+  return (
+    <>
+      <P>Welcome to the grand foyer</P>
+    </>
+  );
+};
+
+export default GrandFoyer;
