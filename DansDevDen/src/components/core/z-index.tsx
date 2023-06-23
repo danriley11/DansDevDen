1e5b15332a70df3929f79311e47b82dff563983f
danriley11 <75771139+danriley11@users.noreply.github.com>
2023-06-23 12:28:28 +1000

    Navigation bar menu (#2)
    * add headlessUI package
    
    * add new colours (greens)
    
    * create z-index constants file
    
    * Update navigation bar menu

---
+++ DansDevDen/src/components/core/z-index.tsx
@@ -0,0 +1,1 @@
+export const MENU_DROPDOWN_Z_INDEX = 10;
---
+++ DansDevDen/src/components/pageStructure/header.tsx/Navigation-Menu.tsx
@@ -0,0 +1,7 @@
+import Menu from '../../menu/Menu';
+
+const NavigationMenu = () => {
+  return <Menu />;
+};
+
+export default NavigationMenu;
