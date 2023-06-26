ef927395d3c99a6809a88b8466376bf5b60a04e9
danriley11 <dan_riley7@outlook.com>
2023-06-26 12:05:19 +1000

    Add new darkGrey colour

--- src/components/core/colours.tsx
+++ src/components/core/colours.tsx
@@ -1,4 +1,5 @@
 export const softGrey = 'RGBA(0, 0, 0, 0.05)';
+export const darkGrey = `RGBA(0, 0, 0, 0.8)`;
 
 export const emeraldGreen = `RGBA(80, 200, 120)`;
 export const deepGreen = `RGBA(0, 85, 28)`;
