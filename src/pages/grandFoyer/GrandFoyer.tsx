2a0e1011039146087a132202d5503fb69c371827
danriley11 <dan_riley7@outlook.com>
2023-06-26 17:20:05 +1000

    Update GrandFoyer render

--- src/pages/grandFoyer/GrandFoyer.tsx
+++ src/pages/grandFoyer/GrandFoyer.tsx
@@ -1,17 +1,76 @@
 import HeroImage from '../../components/images/HeroImage';
-import GrandFoyerImage from '../../assets/GrandFoyer.jpeg';
-import { GrandFoyerHeading } from './GrandFoyer.styles';
-import { Margin } from '../../components/core/spacing';
+
+import CoverImage from '../../assets/CoverImage.jpg';
+import {
+  GrandFoyerFlexContainer,
+  GrandFoyerHeading,
+  GrandFoyerHeadingContainer,
+  GrandFoyerContentContainer,
+  Zone,
+} from './GrandFoyer.styles';
+import { Heading2, Heading3, Heading4, P } from '../../components/core/typography';
+import { Container } from '../../components/pageStructure/Container.styles';
+import { Margin, RightAlign } from '../../components/core/spacing';
+import { FLOOR_PLAN_CONTENT, SPACE_INFO_CONTENT, ZONES_CONTENT } from './GrandFoyer.content';
+import { offWhite } from '../../components/core/colours';
+import FlexDiv from '../../components/blocks/FlexDiv';
 
 const GrandFoyer = () => {
   return (
-    <div>
-      <HeroImage imgSrc={GrandFoyerImage} alt={'temp'} width={'100%'} />
+    <>
+      <HeroImage imgSrc={CoverImage} alt={'temp'} width={'100%'} />
 
-      <Margin smTop={64} mdTop={80} lgTop={128}>
-        <GrandFoyerHeading>Hello, and welcome to the grand foyer.</GrandFoyerHeading>
+      <Margin bottom={128}>
+        <Container>
+          <GrandFoyerHeadingContainer>
+            <GrandFoyerHeading>Hello, </GrandFoyerHeading>
+            <GrandFoyerHeading>and welcome to</GrandFoyerHeading>
+            <GrandFoyerHeading>Dans dev den.</GrandFoyerHeading>
+          </GrandFoyerHeadingContainer>
+
+          <GrandFoyerContentContainer backgroundColour="white">
+            <Heading2>A space for everything</Heading2>
+            <GrandFoyerFlexContainer>
+              <div>
+                <Heading3>{SPACE_INFO_CONTENT.title}</Heading3>
+                {SPACE_INFO_CONTENT.content.map((p) => (
+                  <P>{p}</P>
+                ))}
+                <P>
+                  Please note that this is currently a large work in progress with many, many
+                  features in the pipeline. If you're interested in finding out more about these
+                  features, I would love to introduce you to my <a href="#">Planning suite</a>!
+                </P>
+              </div>
+              <div>
+                <Heading3>{FLOOR_PLAN_CONTENT.title}</Heading3>
+                {FLOOR_PLAN_CONTENT.content.map((p) => (
+                  <P>{p}</P>
+                ))}
+                <RightAlign>
+                  <button>{FLOOR_PLAN_CONTENT.buttonLabel}</button>
+                </RightAlign>
+              </div>
+            </GrandFoyerFlexContainer>
+          </GrandFoyerContentContainer>
+        </Container>
+
+        <GrandFoyerContentContainer backgroundColour={offWhite}>
+          <Container>
+            <Heading2>Den zones</Heading2>
+            <FlexDiv>
+              {ZONES_CONTENT.map((zone) => (
+                <Zone>
+                  <Heading4>{zone.title}</Heading4>
+                  <img src={zone.image} height={192} width={294} />
+                  <P>{zone.blurb}</P>
+                </Zone>
+              ))}
+            </FlexDiv>
+          </Container>
+        </GrandFoyerContentContainer>
       </Margin>
-    </div>
+    </>
   );
 };
 
