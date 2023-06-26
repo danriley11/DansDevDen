8bfe97fc7731db102f0798eaa7080fe531b8b75c
danriley11 <dan_riley7@outlook.com>
2023-06-26 12:12:26 +1000

    Update navbar visuals

--- src/components/pageStructure/header.tsx/Navbar.styles.tsx
+++ src/components/pageStructure/header.tsx/Navbar.styles.tsx
@@ -1,19 +1,44 @@
 import styled from 'styled-components';
-import { softGrey } from '../../core/colours';
+import { darkGrey, softGrey } from '../../core/colours';
+import { MENU_Z_INDEX } from '../../core/z-index';
+import { mediaUp, rem } from '../../core/styles';
 
-export const NavbarContainer = styled.div`
+type NavbarContainerProps = {
+  isScrolled: boolean;
+};
+export const NavbarContainer = styled.div<NavbarContainerProps>`
   display: flex;
   position: sticky;
   top: 0;
   justify-content: space-between;
   flex-wrap: nowrap;
   border-bottom: 1px solid ${softGrey};
+  z-index: ${MENU_Z_INDEX};
+
+  background-color: ${({ isScrolled }) => (isScrolled ? darkGrey : 'transparent')};
 
   & > * {
     align-self: center;
+
+    ${mediaUp.xs`
+    margin: 0 ${rem(8)}
+  `};
+
+    ${mediaUp.sm`
+    margin: 0 ${rem(16)}
+  `};
+
+    ${mediaUp.md`
+    margin: 0 ${rem(24)}
+  `};
+
+    ${mediaUp.lg`
+    margin: 0 ${rem(40)}
+  `};
   }
 `;
 
 export const LogoContainer = styled.div`
+  color: white;
   cursor: pointer;
 `;
--- src/components/pageStructure/header.tsx/Navbar.tsx
+++ src/components/pageStructure/header.tsx/Navbar.tsx
@@ -3,18 +3,33 @@ import { GRAND_FOYER } from '../../../routes/routes';
 import { Heading1 } from '../../core/typography';
 import { LogoContainer, NavbarContainer } from './Navbar.styles';
 import NavigationMenu from './NavigationMenu';
+import { useEffect, useState } from 'react';
 
 export const Navbar = () => {
   const navigate = useNavigate();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      console.log('scrollTop', scrollTop);
+
+      setIsScrolled(!!scrollTop);
+    };
+
+    window.addEventListener(`scroll`, handleScroll);
+
+    return () => {
+      window.removeEventListener(`scroll`, handleScroll);
+    };
+  }, []);
 
   return (
-    <>
-      <NavbarContainer>
-        <LogoContainer onClick={() => navigate(GRAND_FOYER)}>
-          <Heading1>Dans Dev Den</Heading1>
-        </LogoContainer>
-        <NavigationMenu />
-      </NavbarContainer>
-    </>
+    <NavbarContainer isScrolled={isScrolled}>
+      <LogoContainer onClick={() => navigate(GRAND_FOYER)}>
+        <Heading1>Dans Dev Den</Heading1>
+      </LogoContainer>
+      <NavigationMenu />
+    </NavbarContainer>
   );
 };
