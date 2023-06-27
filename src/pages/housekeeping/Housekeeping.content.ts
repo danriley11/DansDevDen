3c65884c168f59ad64f217fc9805dad2efde931e
danriley11 <dan_riley7@outlook.com>
2023-06-27 19:23:15 +1000

    Create housekeeping content

---
+++ src/pages/housekeeping/Housekeeping.content.ts
@@ -0,0 +1,37 @@
+import AboutMeImage from '../../assets/AboutMeImage.webp';
+
+export const HOUSEKEEPING_CONTENT = [
+  {
+    title: `My reason`,
+    text: [
+      `My purpose for creating this website has a lot to do with my innate love for not only content creation and story telling, but to provide others with a space that I never personally had (or was aware of) during my journey into development. I always found that other websites just had a great amount of detail on a topic, but it wouldn't be until I was learning or researching that topic or explicit attribute that I would come across it. The biggest challenge for me is finding my way through the endless sea of knowledge and content without a mentor or teacher, and that is what I've made this website for.`,
+    ],
+    list: null,
+    image: AboutMeImage,
+    imageAlt: `Temp alt`,
+  },
+  {
+    title: `My background`,
+    text: [
+      `Born in England, raised by Irish, living in Australia. Lover of all things tech and quite creative which has always made it difficult for me to find something to specialise in. As a result I've landed as a fullstack developer with a stronger skillset in frontend development. My ultimate dream would be providing freelance services to small businesses and helping them get off the ground, however while I'm learning and growing this may change and I'm most excited to work in larger companies currently to gain knowledge and experience I otherwise wouldn't have the exposure and opportunity for.`,
+    ],
+    list: null,
+    image: AboutMeImage,
+    imageAlt: `Temp alt`,
+  },
+  {
+    title: `My timeline`,
+    text: null,
+    list: [
+      `Discovered my love for IT during highschool.`,
+      `Enrolled for Info.Systems & Business Analysis for university.`,
+      `Slowed university to take on my first corporate role in DevOps.`,
+      `Resigned from DevOps to conclude university studies.`,
+      `Landed my first Software Engineer role as a contractor.`,
+      `Bogged down with the world through COVID.`,
+      `Landed another contract role as a software engineer to then pivot into full-time at a proptech startup.`,
+    ],
+    image: AboutMeImage,
+    imageAlt: `Temp alt`,
+  },
+];
