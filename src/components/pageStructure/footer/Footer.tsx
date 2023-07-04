4e1ab07c85486f0f8b7d1bc3a7407dd96e136f3f
danriley11 <dan_riley7@outlook.com>
2023-07-04 19:55:30 +1000

    Update Footer component

--- src/components/pageStructure/footer/Footer.tsx
+++ src/components/pageStructure/footer/Footer.tsx
@@ -1,11 +1,14 @@
 import dayjs from 'dayjs';
 import { P } from '../../core/typography';
 import { FooterContainer } from './Footer.styles';
+import { Container } from '../Container.styles';
 
 const Footer = () => {
   return (
     <FooterContainer as="footer">
-      <P>Copyright © {dayjs().year()} Dans Dev Den - All Rights Reserved.</P>
+      <Container>
+        <P>Copyright © {dayjs().year()} Dans Dev Den - All Rights Reserved.</P>
+      </Container>
     </FooterContainer>
   );
 };
