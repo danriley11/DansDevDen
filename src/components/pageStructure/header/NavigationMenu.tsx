4fc761655263bd08e877b2ce0ef5fcb57fce203b
danriley11 <dan_riley7@outlook.com>
2023-07-27 09:57:34 +1000

    update menu content & styling

--- src/components/pageStructure/header/NavigationMenu.tsx
+++ src/components/pageStructure/header/NavigationMenu.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
-import { HOUSEKEEPING, PLANNING, SHOWCASING } from '../../../routes/routes';
+import { GRAND_FOYER, HOUSEKEEPING, PLANNING, SHOWCASING } from '../../../routes/routes';
 import { MenuIconContainer, XBar1, XBar2, XBar3 } from '../../menu/Menu.styles';
 import { MenuDropdownContainer, MenuDropdownItem } from '../../menu/MenuDropdown.styles';
 import { P } from '../../core/typography';
+import { useLocation } from 'react-router-dom';
 
 const NavigationMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const navigate = useNavigate();
+  const location = useLocation();
 
   const toggleHamburger = () => {
     setIsOpen(!isOpen);
   };
+
+  function pathMatch(path: string): boolean {
+    if (path === location.pathname) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   return (
     <>
@@ -25,6 +35,16 @@ const NavigationMenu = () => {
       {isOpen && (
         <MenuDropdownContainer>
           <MenuDropdownItem
+            isPath={pathMatch(GRAND_FOYER)}
+            onClick={() => {
+              toggleHamburger();
+              navigate(GRAND_FOYER);
+            }}>
+            <P>Grand foyer</P>
+          </MenuDropdownItem>
+
+          <MenuDropdownItem
+            isPath={pathMatch(HOUSEKEEPING)}
             onClick={() => {
               toggleHamburger();
               navigate(HOUSEKEEPING);
@@ -33,6 +53,7 @@ const NavigationMenu = () => {
           </MenuDropdownItem>
 
           <MenuDropdownItem
+            isPath={pathMatch(SHOWCASING)}
             onClick={() => {
               toggleHamburger();
               navigate(SHOWCASING);
@@ -41,6 +62,7 @@ const NavigationMenu = () => {
           </MenuDropdownItem>
 
           <MenuDropdownItem
+            isPath={pathMatch(PLANNING)}
             onClick={() => {
               toggleHamburger();
               navigate(PLANNING);
