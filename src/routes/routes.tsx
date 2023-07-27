3aa3ce91b904d428841092a568b54e4c5f2cdd8b
danriley11 <dan_riley7@outlook.com>
2023-07-27 10:11:25 +1000

    Create Learning route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,3 +2,4 @@ export const GRAND_FOYER = '/';
 export const HOUSEKEEPING = '/housekeepers-lodge';
 export const SHOWCASING = '/showcasing-suite';
 export const PLANNING = '/planning-suite';
+export const LEARNING = '/learning-suite';
