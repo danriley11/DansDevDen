7ab381023862e8ac56ec71768642160b9b58a18c
danriley11 <dan_riley7@outlook.com>
2023-10-23 17:15:58 +1100

    Add 'disabled' prop to ButtonTab

--- src/components/buttons/ButtonTab.tsx
+++ src/components/buttons/ButtonTab.tsx
@@ -3,14 +3,19 @@ import { ButtonLink } from './Buttons.styles';
 type ButtonTabProps = {
   url: string;
   buttonText: string;
+  disabled?: boolean;
 };
-const ButtonTab = ({ url, buttonText }: ButtonTabProps) => {
+const ButtonTab = ({ url, buttonText, disabled }: ButtonTabProps) => {
   const handleButtonClick = () => {
     const urlLocation = url; // Specify the URL you want to open
     window.open(urlLocation, '_blank');
   };
 
-  return <ButtonLink onClick={handleButtonClick}>{buttonText}</ButtonLink>;
+  return (
+    <ButtonLink onClick={handleButtonClick} disabled={disabled}>
+      {buttonText}
+    </ButtonLink>
+  );
 };
 
 export default ButtonTab;
