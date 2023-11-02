a55567c469fb30ad4c77609d69112f33ebb2fe13
danriley11 <75771139+danriley11@users.noreply.github.com>
2023-11-02 17:01:23 +1100

    Resolve Vercel error404 on new image sources (#25)
    * Resolve Vercel error404 on new image sources
    
    * cleanup on PlanningSuite

--- src/components/pageStructure/footer/Footer.tsx
+++ src/components/pageStructure/footer/Footer.tsx
@@ -4,6 +4,9 @@ import { FooterContainer } from './Footer.styles';
 import { Container } from '../Container.styles';
 import { Link } from 'react-router-dom';
 import ButtonTab from '../../buttons/ButtonTab';
+import GitHubLogo from '../../../assets/GitHubLogo.png';
+import LinkedInLogo from '../../../assets/LinkedInLogo.png';
+import ResumeIcon from '../../../assets/ResumeIcon.png';
 
 const Footer = () => {
   return (
@@ -30,14 +33,14 @@ const Footer = () => {
         <Container>
           <Heading4>Socials</Heading4>
           <div className="footer-socials">
-            <ButtonTab url="https://github.com/danriley11" buttonText={<img src="/src/assets/GitHubLogo.png" width="32px" />} />
+            <ButtonTab url="https://github.com/danriley11" buttonText={<img src={GitHubLogo} width="32px" />} />
             <ButtonTab
               url="https://www.linkedin.com/in/daniel-riley-a0b43415a/"
-              buttonText={<img src="/src/assets/LinkedInLogo.png" width="32px" />}
+              buttonText={<img src={LinkedInLogo} width="32px" />}
             />
             <ButtonTab
               url="https://www.canva.com/design/DAFyOF4C9_A/JxTgvBaS67HPB4iU3pMHlw/view?utm_content=DAFyOF4C9_A&utm_campaign=designshare&utm_medium=link&utm_source=editor#1"
-              buttonText={<img src="/src/assets/ResumeIcon.png" width="32px" />}
+              buttonText={<img src={ResumeIcon} width="32px" />}
             />
           </div>
         </Container>
