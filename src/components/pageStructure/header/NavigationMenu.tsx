2c00c4d0f209a2959fdc903674f24b293b439a84
danriley11 <dan_riley7@outlook.com>
2024-01-18 11:06:05 +1100

    linting

--- src/components/pageStructure/header/NavigationMenu.tsx
+++ src/components/pageStructure/header/NavigationMenu.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
-import { GRAND_FOYER, HOUSEKEEPING, LEARNING, PLANNING, SHOWCASING } from '../../../routes/routes';
+import { GRAND_FOYER, HOUSEKEEPING, LEARNING, /*PLANNING,*/ SHOWCASING } from '../../../routes/routes';
 import { MenuIconContainer, XBar1, XBar2, XBar3 } from '../../menu/Menu.styles';
 import { MenuDropdownContainer, MenuDropdownItem } from '../../menu/MenuDropdown.styles';
 import { P } from '../../core/typography';
