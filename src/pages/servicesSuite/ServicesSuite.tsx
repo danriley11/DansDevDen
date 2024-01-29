f6e3b5adfd0ba2e2fd507ea4cbf01d841d71f985
danriley11 <dan_riley7@outlook.com>
2024-01-29 19:07:11 +1100

    Create ServicesSuite

--- src/pages/servicesSuite/ServicesSuite.tsx
+++ src/pages/servicesSuite/ServicesSuite.tsx
@@ -1,6 +1,9 @@
-import { Heading1 } from '../../components/core/typography';
+import { Heading1, P } from '../../components/core/typography';
 import { Container } from '../../components/pageStructure/Container.styles';
 import { NavBacking } from '../../components/pageStructure/header/Navbar.styles';
+import Service from './Service';
+import SERVICES_LIST from './ServicesSuite.constants';
+import ServicesSuiteWrapper from './ServicesSuite.styles';
 
 const ServicesSuite = () => {
   return (
@@ -8,6 +11,19 @@ const ServicesSuite = () => {
       <NavBacking />
 
       <Heading1>Services Suite</Heading1>
+
+      <ServicesSuiteWrapper>
+        <P>
+          Explore the realm of tailored services designed just for you! Whether it's enhancing your website, resolving issues,
+          or embarking on a digital journey for your business, you're in the right place. Discover the perfect service for your
+          needs, and if you can't find what you're looking for, don't hesitate to reach out. Your unique requirements are at the
+          heart of what I do!
+        </P>
+
+        {SERVICES_LIST.map((service, index) => (
+          <Service service={service} key={index} />
+        ))}
+      </ServicesSuiteWrapper>
     </Container>
   );
 };
